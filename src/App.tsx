3f232d537405467e46ea6579151bfa96e8b5856c
Jarrett Dougherty <jarrettdougherty@gmail.com>
2023-12-06 11:34:46 -0500

    experiment with map

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
 import { ReactElement } from "react";
-import { WelcomeObj } from "./interfaces/types";
+import { BookListItem, WelcomeObj } from "./interfaces/types";
+
+const list: BookListItem[] = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan White',
+    num_comments: 3,
+    points: 4,
+    objectID: 0
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1
+  }
+];
 
 const welcome: WelcomeObj = {
-  greeting: "Hey",
-  title: "React"
+  greeting: 'Hey',
+  title: 'React'
 };
 
 function getTitle(title: string): string {
@@ -15,9 +34,24 @@ function App(): ReactElement {
   return (
     <div>
       <h1>{welcome.greeting} {welcome.title}</h1>
-      <h1>{getTitle("Hello World")}</h1>
+      <h1>{getTitle('Hello World')}</h1>
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" />
+
+      <hr />
+
+      <ul>
+        {list.map((item: BookListItem): ReactElement => (
+          <li key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title} </a>
+            </span>
+            <span>{item.author} </span>
+            <span>{item.num_comments} </span>
+            <span>{item.points}</span>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
--- src/interfaces/types.ts
+++ src/interfaces/types.ts
@@ -1,3 +1,12 @@
+export interface BookListItem {
+  title: string;
+  url: string;
+  author: string;
+  num_comments: number;
+  points: number;
+  objectID: number;
+}
+
 export interface WelcomeObj {
     greeting: string;
     title: string;
